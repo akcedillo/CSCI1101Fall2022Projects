6fc1a43b074002ef3f85ed73fb8973419a3c7227
akcedillo <115722870+akcedillo@users.noreply.github.com>
2022-11-22 22:59:32 -0600

    Add files via upload

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (897438698274, 1, 'Hello there');
+
+--UPDATE sandbox SET double_value = -2.47836482 WHERE id = 139;
+
+--DELETE FROM sandbox WHERE id = 100;
+
+SELECT * FROM sandbox;
